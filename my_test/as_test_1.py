3347790266382b978753926f3b3171ea9b9e04b9
mskz1 <mskz.tsuji@me.com>
2018-01-21 14:51:41 +0900

    修正など

--- my_test/as_test_1.py
+++ my_test/as_test_1.py
@@ -4,8 +4,8 @@ from anastruct.fem.system import SystemElements
 mdl = SystemElements()
 
 # add beam element
-mdl.add_element([[0, 0], [2, 0]], EA=50, EI=100)
-mdl.add_element([[2, 0], [4, 0]], EA=50, EI=100)
+mdl.add_element([[0, 0], [6, 0]], EA=50, EI=100)
+mdl.add_element([[6, 0], [8, 0]], EA=50, EI=100)
 
 # add nodal load
 mdl.point_load(2, 0, -10)
@@ -16,18 +16,23 @@ mdl.add_support_roll(3)
 
 # output plot
 mdl.solve()
-
-mdl.show_structure(verbosity=0, scale=1)
-mdl.show_bending_moment(verbosity=0, scale=1)
-mdl.show_shear_force()
-mdl.show_displacement(verbosity=0, scale=1)
+w,h = 5,4
+# mdl.show_structure(verbosity=0, scale=1,figsize=(w,h))
+# mdl.show_bending_moment(verbosity=0, scale=1,figsize=(w,h))
+# mdl.show_shear_force(figsize=(w,h))
+# mdl.show_displacement(verbosity=0, scale=1,figsize=(w,h))
 
 print('{:-^80}'.format('Nodal Result') )
-
-
 node_res = mdl.get_node_results_system()
+print(node_res)
+
 for x in node_res:
     print('NodeID={:5} ,Fx={:8.3f} ,Fz={:8.3f} ,Ty={:8.3f} ,ux={:8.3f} ,uz={:8.3f} ,phi_y={:8.3f}'.format(*x))
+
+print('-----')
+print(mdl.get_node_results_system(node_id=2))
+
+
 print('{:-^80}'.format('Elemental Result') )
 
 elem_list = mdl.element_map.keys()
